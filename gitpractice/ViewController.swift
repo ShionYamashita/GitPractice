9666597571d62dbf55d62f96eabc0bf24704decd
Yoshiko Yamashita <sy0624.sora@gmail.com>
2015-04-12 16:04:30 +0900

    first commit

---
+++ gitpractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitpractice
+//
+//  Created by Yoshiko Yamashita on 2015/04/12.
+//  Copyright (c) 2015年 Shion Yamashita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
